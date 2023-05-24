5fb2554ab23501d51d5ef850cb0d40ffe6b0cecf
Ahmed Nasr <a.nasr.me@gmail.com>
2023-05-24 06:15:11 +0300

    feat: add data loading functionality to load-movies endpoint

--- movies_mini_etl/movies/api/views.py
+++ movies_mini_etl/movies/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 
 from .serializers import MoviesLoadingSerializer
+from movies_mini_etl.movies.utils import WikidataMoviesLoader
 
 logger = logging.getLogger(__name__)
 
@@ -17,5 +18,6 @@ class LoadMoviesAPIView(GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         movies_count = serializer.validated_data.get("movies_count", 5)
-        # TODO: Add data loading functionality
+
+        WikidataMoviesLoader.load_movies(movies_count)
         return Response(status=status.HTTP_200_OK)
