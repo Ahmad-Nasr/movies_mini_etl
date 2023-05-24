e8b2ccc6e3d221ae8516478b8416f72f9aeaa130
Ahmed Nasr <a.nasr.me@gmail.com>
2023-05-24 05:49:17 +0300

    feat: add Movie, Director, Actor, and Genre models

--- movies_mini_etl/movies/models.py
+++ movies_mini_etl/movies/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class Movie(TimeStampedModel):
+    title = models.CharField(_('Title'), max_length=128)
+    wikidata_id = models.PositiveIntegerField(_('Wikidata ID'))
+    imdb_id = models.CharField(_('IMDB ID'), max_length=128)
+
+    def __str__(self):
+        return self.title
+
+
+class Director(TimeStampedModel):
+    name = models.CharField(_('Name'), max_length=128)
+    wikidata_id = models.PositiveIntegerField(_('Wikidata ID'))
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(TimeStampedModel):
+    name = models.CharField(_('Name'), max_length=128)
+    wikidata_id = models.PositiveIntegerField(_('Wikidata ID'))
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(TimeStampedModel):
+    name = models.CharField(_('Name'), max_length=128)
+
+    def __str__(self):
+        return self.name
